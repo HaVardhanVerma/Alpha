5aa6aeadb649d5494415459be880ededfba12afe
Harsh Vardhan Verma <harshvardhanve100@gmail.com>
2025-06-30 14:59:17 +0530

    git commit -m "Update Appwrite database ID in conversation and app actions"

--- src/lib/appwriter.js
+++ src/lib/appwriter.js
@@ -1,7 +1,9 @@
 import { Client, Account, Avatars, Databases } from 'appwrite';
 
 const client = new Client();
-client.setEndpoint('https://cloud.appwrite.io/v1').setProject('fusion5');
+client
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
 
